bb13ae974d1532b3f1a4b4067f39bc73a68e977f
Nikhil Jolly <nikhiljolly@Nikhils-Mac-mini.local>
2016-08-31 01:34:27 +0530

    0.2
    ✔ Decrease opacity to 0.5
    ✔ Remove box-shadow
    ✔ Scrolling effect to nav links
    ✔ Home parallax image replaced with modified image to remove dependency
    on opacity & bg color (set opacity back to 1).
    ✔ Clean css using navbar id instead of class structure
    
    Basic wireframe:
      Homepage aka Bio
         ✔ Parllax1
         ✔ Quote box
         ✔ Bio
      Portfolio
         ✔ 3 - 2 - 3.. grid style
         ✔ Temporarily added a few portfolio items
      ✔ Tab functionality for nav links
         ✔ Smooth transitions
         ✔ Top margin for all other content (projects, journal..) except
    bio with parallax.
         ✔ Removed top margin and set navbar to static positioning.
    
    ✔ Reset navbar to static positioning (to avoid many bugs) and give home
    content negative top margin instead.
    ✔ Changed navbar padding to margin
    ✔ Workaround for navbar opening and content moving downwards on small
    screens while on home page.
    ✔ Moved navbar to absolute positioning so that it overlaps the content
    instead of pushing it down.
    ✔ Set z-index of navbar to 1 so that it appears above everything.
    ✔ Changed navbar back to padding due to bug on full width.
    ✔ Removed top margin for home as it isn't necessary anymore.
    ✔ Top margin for all other content except home, using below-navbar
    class.

---
+++ assets/js/scroll.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+	$('a[href^="#"]').on('click',function (e) {
+	    e.preventDefault();
+      $('html,body').animate({scrollTop:$(this.hash).offset().top}, 500);
+	});
+});
