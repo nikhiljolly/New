fe10d320dc854249eac1f52c2d41a472d30bfe8c
Nikhil Jolly <nikhiljolly@Nikhils-Mac-mini.local>
2016-09-09 16:28:33 +0530

    0.2.3
    Portfolio
        ✔ Same size of portfolio items?
        ✔ 4:3 aspect ratio of portfolio items
        ✔ Covers
    
    ✔ Change url on changing tab
    ✔ Open via # url
    ✔ Style navbar active class
    ✔ Removed scroll.js and added navhash js
    ✔ navhash js for url changing on nav links & opening url directly via
    nav links

--- assets/js/scroll.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function(){
-	$('a[href^="#"]').on('click',function (e) {
-	    e.preventDefault();
-      $('html,body').animate({scrollTop:$(this.hash).offset().top}, 500);
-	});
-});
